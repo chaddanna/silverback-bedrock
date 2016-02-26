dfbba9a0ed25eddbc554965d2c75c568110cf9e8
Chad Danna <chad@formulateddesign.com>
2016-02-26 15:41:59 -0600

    Add Custom Fields to template >Brought the hero background to inline so it can be changed in the CMS

--- web/app/themes/silverback/template-home.php
+++ web/app/themes/silverback/template-home.php
@@ -6,35 +6,35 @@
 
 <?php get_template_part('templates/page', 'header'); ?>
   <main id="main">
-    <section id="hero">
+    <section id="hero" style="background: url('<?php the_field('hero_image');?>') no-repeat top center fixed;-webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
       <div class="hero-content">
-        <h1>Silverback Advertising is a full-service automotive ad agency.</h1>
-        <a href="#contact">Work With Us</a>
+        <h1><?php the_field('hero_text');?></h1>
+        <a href="#contact"><?php the_field('hero_cta_text');?></a>
+      </div>
+
+      <div class="trim">
+        <img src="<?= get_template_directory_uri(); ?>/dist/images/trim.png" alt="">
       </div>
     </section>
-
-    <div class="trim">
-      <img src="<?= get_template_directory_uri(); ?>/dist/images/trim.png" alt="">
-    </div>
 
     <section id="services">
       <div class="container">
         <div class="row">
+          <?php
+            if( have_rows('services_repeater') ):
+            while ( have_rows('services_repeater') ) : the_row();
+          ?>
+
           <div class="col col-3">
-            <img src="img/digital.svg" alt="">
-            <h4>Digital</h4>
-            <p>Online marketing means delivering results to our clients. Our digital department has a track record of success, focusing on targeted campaigns that drive quality traffic to your website.</p>
+            <img src="<?php the_sub_field('service_icon');?>" alt="">
+            <h4><?php the_sub_field('service_title');?></h4>
+            <p><?php the_sub_field('service_content');?></p>
           </div>
-          <div class="col col-3">
-            <img src="img/creative.svg" alt="">
-            <h4>Creative</h4>
-            <p>Compelling copy combined with vivid visuals is our recipe for top-notch creative.  Our team of in-house designers, editors, writers, producers, directors, and photographers are fully equipped to take your next creative project from concept to completion.</p>
-          </div>
-          <div class="col col-3">
-            <img src="img/media.svg" alt="">
-            <h4>Media</h4>
-            <p>When it comes to media buying, one thing matters: experience.  Our media buying team has X,XXX years of automotive ad buying experience, which ensures you get best-in-market pricing no matter the programming.</p>
-          </div>
+
+          <?php
+            endwhile;
+            endif;
+          ?>
         </div>
       </div>
     </section>
@@ -43,60 +43,50 @@
       <div class="row clearfix">
         <div class="content">
           <div>
-            <h2>Creative</h2>
+            <h2><?php the_field('creative_title');?></h2>
             <p>
-              Compelling copy combined with vivid visuals is our recipe for top-notch creative.  Our team of in-house designers, editors, writers, producers, directors, and photographers are fully equipped to take your next creative project from concept to completion.
+              <?php the_field('creative_content');?>
             </p>
+            <a class="reel-cta">See Demo Reel</a>
           </div>
         </div>
         <div class="video">
-          <div class="video-placeholder" data-video="https://player.vimeo.com/video/128313782?title=0&byline=0&portrait=0">
-            <img src="img/video-placeholder.png" alt="">
+          <div class="video-placeholder" data-video="<?php the_field('reel_video');?>">
+            <img src="<?php the_field('reel_image');?>" alt="">
           </div>
         </div>
       </div>
       <div class="row">
         <div class="work clearfix">
-          <div class="work-item" data-video="https://www.youtube.com/embed/-R_483zeVF8">
+          <?php
+            if( have_rows('creative_projects_repeater') ):
+            while ( have_rows('creative_projects_repeater') ) : the_row();
+          ?>
+
+          <div class="work-item" data-video="<?php the_sub_field('video_url');?>">
             <div class="work-meta">
-              <h4>Work Title</h4>
-              <h5>Work Type</h5>
+              <h4><?php the_sub_field('project_title');?></h4>
+              <h5><?php the_sub_field('project_type');?></h5>
             </div>
-            <img src="img/work-item1.png" alt="" />
+            <img src="<?php the_sub_field('thumbnail_image');?>" alt="" />
           </div>
-          <div class="work-item" data-video="https://www.youtube.com/embed/V50vifuTx2Y">
-            <div class="work-meta">
-              <h4>Work Title</h4>
-              <h5>Work Type</h5>
-            </div>
-            <img src="img/work-item2.png" alt="" />
-          </div>
-          <div class="work-item" data-video="https://www.youtube.com/embed/LT6n1HcJOio">
-            <div class="work-meta">
-              <h4>Work Title</h4>
-              <h5>Work Type</h5>
-            </div>
-            <img src="img/work-item3.png" alt="" />
-          </div>
-          <div class="work-item" data-video="https://www.youtube.com/embed/LbBiCL5-SAY">
-            <div class="work-meta">
-              <h4>Work Title</h4>
-              <h5>Work Type</h5>
-            </div>
-            <img src="img/work-item4.png" alt="" />
-          </div>
+
+          <?php
+            endwhile;
+            endif;
+          ?>
         </div>
       </div>
     </section>
 
     <section id="quote">
-      <h4>"Good design is making something intelligible and memorable. Great design is making something memorable and meaningful."</h4>
-      <h5>Dieter Rams</h5>
+      <h4><?php the_field('quote_text');?></h4>
+      <h5><?php the_field('quotee');?></h5>
     </section>
 
     <section id="blog">
       <div class="container">
-        <h2>Our Blog & Case Studies</h2>
+        <h2><?php the_field('blog_title');?></h2>
         <div class="row clearfix">
 
         <?php
@@ -111,87 +101,65 @@
             <a href="">Read More</a>
           </div>
 
-        <?php endwhile; ?>
+    		<?php endwhile; wp_reset_query(); ?>
 
         </div>
         <div class="row cta-container">
-          <a href="#">View Our Blog</a>
+          <a href="<?php the_field('blog_cta_page_link');?>"><?php the_field('blog_cta_text');?></a>
         </div>
       </div>
     </section>
 
     <section id="team">
       <div class="container">
-        <h2>Our Leadership Team</h2>
+        <h2><?php the_field('team_title');?></h2>
         <div class="row clearfix">
+          <?php
+            if( have_rows('team_member_repeater') ):
+            while ( have_rows('team_member_repeater') ) : the_row();
+          ?>
+
           <div class="col col-3 team-item">
             <figure>
-              <img src="img/jeff-cossar.png" alt="">
+              <img src="<?php the_sub_field('team_member_image');?>" alt="">
             </figure>
-            <h4>Jeff Cossar</h4>
-            <p>President</p>
-            <span>1-123-456-7890</span>
-            <a href="">Email Me</a>
+            <h4><?php the_sub_field('team_member_name');?></h4>
+            <p><?php the_sub_field('team_member_position');?></p>
+            <span><?php the_sub_field('team_member_phone');?></span>
+            <a href="mailto:<?php the_sub_field('email');?>">Email Me</a>
           </div>
-          <div class="col col-3 team-item">
-            <figure>
-              <img src="img/shane-stender.png" alt="">
-            </figure>
-            <h4>Shane Stender</h4>
-            <p>Managing Director</p>
-            <span>1-123-456-7890</span>
-            <a href="">Email Me</a>
-          </div>
-          <div class="col col-3 team-item">
-            <figure>
-              <img src="img/esther-menard.png" alt="">
-            </figure>
-            <h4>Esther Menard</h4>
-            <p>Account Service Director</p>
-            <span>1-123-456-7890</span>
-            <a href="">Email Me</a>
-          </div>
+
+          <?php
+            endwhile;
+            endif;
+          ?>
         </div>
       </div>
 
       <section id="locations">
         <div class="row">
+          <?php
+            if( have_rows('location_repeater') ):
+            while ( have_rows('location_repeater') ) : the_row();
+          ?>
+
           <div class="col col-3 no-gutter location-item">
             <figure>
-              <img src="img/location-item1.png" alt="">
+              <img src="<?php the_sub_field('location_image');?>" alt="">
             </figure>
             <div class="location-meta">
-              <h3>Baton Rouge</h3>
-              <h5>Corporate Headquarters</h5>
-              <h5>10538 Kentshire Court<br>Baton Rouge, Louisiana</h5>
-              <h5>P: 800-XXX-XXXX<br>F: 225-XXX-XXXX</h5>
-              <a class="mobile-visible" href="">Call us</a>
+              <h3><?php the_sub_field('city');?></h3>
+              <h5><?php the_sub_field('location_type');?></h5>
+              <h5><?php the_sub_field('street_address');?><br><?php the_sub_field('city');?>, <?php the_sub_field('state');?></h5>
+              <h5>P: <?php the_sub_field('phone_number');?><br>F: <?php the_sub_field('fax_number');?></h5>
+              <a class="mobile-visible" href="tel:<?php the_sub_field('phone_number');?>">Call us</a>
             </div>
           </div>
-          <div class="col col-3 no-gutter location-item">
-            <figure>
-              <img src="img/location-item2.png" alt="">
-            </figure>
-            <div class="location-meta">
-              <h3>Charlotte</h3>
-              <h5>East Coast Ops</h5>
-              <h5>10538 Kentshire Court<br>Baton Rouge, Louisiana</h5>
-              <h5>P: 800-XXX-XXXX<br>F: 225-XXX-XXXX</h5>
-              <a class="mobile-visible" href="">Call us</a>
-            </div>
-          </div>
-          <div class="col col-3 no-gutter location-item">
-            <figure>
-              <img src="img/location-item3.png" alt="">
-            </figure>
-            <div class="location-meta">
-              <h3>Seattle</h3>
-              <h5>West Coast Ops</h5>
-              <h5>10538 Kentshire Court<br>Baton Rouge, Louisiana</h5>
-              <h5>P: 800-XXX-XXXX<br>F: 225-XXX-XXXX</h5>
-              <a class="mobile-visible" href="">Call us</a>
-            </div>
-          </div>
+
+          <?php
+            endwhile;
+            endif;
+          ?>
         </div>
         <div class="clearfix"></div>
       </section>
